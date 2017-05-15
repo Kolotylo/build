1c44936e6774fdc1039fec5e6f4f4a2918eba3b7
Kolotylo <kolot80@gmail.com>
2017-05-15 16:51:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/products/:id', function (req, res){
   res.send(products[req.params.id]);
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 1338;
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
